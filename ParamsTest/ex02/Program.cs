ca80c337bd6cf7e0f15d14a5debeaf5342306601
KangDongYoon <davidkang8111@gmail.com>
2019-01-06 10:49:18 +0900

    두번째 C# App

---
+++ ParamsTest/ex02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Minus(1, 2));
+            Console.WriteLine(Minus(1));
+            Console.WriteLine(Minus(i: 8, j: 9));
+            Console.WriteLine(Minus(j: 87, i: 9));
+            Console.WriteLine(Minus());
+        }
+
+        static int Minus(int i=0, int j = 0)
+        {
+            return i - j;
+        }
+    }
+}
---
+++ Property/Property/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace PropertyTest2
+{
+    class Emp : System.Object
+    {
+        private string name;
+        /*public void SetName(string name) { this.name = name+"님"; }  //세터, setter
+        public string GetName() { return this.name; }   //게터, getter*/
+
+        public string Name  //일반속성
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value + "님";
+            }
+        }
+
+        public int Sabun    //자동구현속성
+        {
+            get;
+            set;
+        }
+
+        public override string ToString()
+        {
+            return "[사원]" + Name + Sabun;
+        }
+
+    }
+
+    class EmpTest
+    {
+        static void Main()
+        {
+            Emp e = new Emp();
+            e.Name = "홍";   //L.Value
+            e.Sabun = 1004;
+            string s = e.Name;
+            Console.WriteLine(e.Name);  //R.Value
+            Console.WriteLine(e);
+        }
+    }
+}
