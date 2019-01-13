b3c18c8e5e779fd663e3bcec758bf0eaaaadcbe9
KangDongYoon <davidkang8111@gmail.com>
2019-01-13 18:05:38 +0900

    나의 세번째 C# App

---
+++ Thread/Lab1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace lab1
+{
+    class ThreadTest
+    {
+        static void Thmethod()
+        {
+            int id = AppDomain.GetCurrentThreadId();
+            Console.WriteLine("Thread[{0}] Thmethod Method Running", id);
+        }
+
+        static void Main()
+        {
+            int id = AppDomain.GetCurrentThreadId();
+            Console.WriteLine("Main Thread[{0}]", id);
+
+            for(int i = 0; i < 10; i++)
+            {
+                Thread th = new Thread(new ThreadStart(Thmethod));
+                th.Start();
+            }
+        }
+    }
+}
---
+++ Thread/Thread/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+public class ThreadTest
+{
+    public void FirstWork(object name)
+    {
+        Console.Write(name);
+
+        for(int i = 0; i < 100; i++)
+        {
+            Thread.Sleep(1000); //밀리세컨드 단위
+            Console.Write($"F{i}");
+        }
+    }
+
+    public void SecondWork(object name)
+    {
+        Console.Write(name);
+
+        for(int i = 0; i < 100; i++)
+        {
+            Thread.Sleep(1000);
+            Console.Write($"S{i}");
+        }
+    }
+
+    [MTAThread]
+    public static void Main()
+    {
+        ThreadTest t = new ThreadTest();
+
+        Thread first = new Thread(t.FirstWork);
+        Thread second = new Thread(new ParameterizedThreadStart(t.SecondWork));
+
+        first.Priority = ThreadPriority.Lowest;
+        second.Priority = ThreadPriority.Highest;
+
+        first.Start("First");
+        second.Start("Second");
+    }
+}
